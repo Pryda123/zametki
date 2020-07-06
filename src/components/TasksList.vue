43605b1f05f4f3a17e7c2893f8261f9ab5ee59d4
Pryda123 <44433921+Pryda123@users.noreply.github.com>
2020-07-06 10:51:41 +0300

    add header

--- src/components/TasksList.vue
+++ src/components/TasksList.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="tasks-container">
+        <h2 v-if="!tasks.length" class="task-header px-15">Добавьте свою первую заметку...</h2>
         <task-card v-for="task in tasks" :key="task.id" :task="task" v-on:openmodal="openmodal"></task-card>
         <modal ref="modal" v-if="modal" v-on:closemodal="closemodal" v-on:deletetask="deletetask"></modal>
     </div>
@@ -53,5 +54,9 @@ export default {
   .tasks-container {
     padding: 30px 0;
   }
+}
+.px-15 {
+    padding-left: 15px;
+    padding-right: 15px;
 }
 </style>
